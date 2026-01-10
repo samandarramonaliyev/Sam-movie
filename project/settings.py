71b3d8a32a00c8719b565434e010b7a630bcf31a
samandar-pixel <samandarrahmonaliev3005@gmail.com>
2026-01-10 18:12:19 +0500

    Anonymous movie handling: admin checkbox reflects user choice

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)_8&k!7l+1+l_1#vd*#_(_vp176iv+fud!6t+nfw+hgtsg-ho)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
